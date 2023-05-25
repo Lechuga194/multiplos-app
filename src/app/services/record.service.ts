c1604636d6723c7974a13741ba35ec37ce5e17e2
Lechuga194 <joselechuga194@gmail.com>
2023-05-25 09:40:06 -0600

    Fix: Adds missing try/catch clause

--- src/app/services/record.service.ts
+++ src/app/services/record.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { collection, Firestore, addDoc, collectionData, doc, deleteDoc } from '@angular/fire/firestore';
 import { Multiple } from '@app/interfaces/multiple';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -9,17 +9,32 @@ export class RecordService {
   constructor(private firestore: Firestore) {}
 
   saveRecord(multipleData: Multiple) {
-    const recordRef = collection(this.firestore, 'record');
-    return addDoc(recordRef, multipleData);
+    try {
+      const recordRef = collection(this.firestore, 'record');
+      return addDoc(recordRef, multipleData);
+    } catch (e) {
+      console.error(e, 'An error occurred while saving the record');
+      return throwError(e);
+    }
   }
 
   getRecords(): Observable<Multiple[]> {
-    const recordRef = collection(this.firestore, 'record');
-    return collectionData(recordRef, { idField: 'id' }) as Observable<Multiple[]>;
+    try {
+      const recordRef = collection(this.firestore, 'record');
+      return collectionData(recordRef, { idField: 'id' }) as Observable<Multiple[]>;
+    } catch (e) {
+      console.error(e, 'An error getting the records');
+      return throwError(e);
+    }
   }
 
   deleteRecord(id: string) {
-    const recordDocRef = doc(this.firestore, `record/${id}`);
-    return deleteDoc(recordDocRef);
+    try {
+      const recordDocRef = doc(this.firestore, `record/${id}`);
+      return deleteDoc(recordDocRef);
+    } catch (e) {
+      console.error(e, 'An error occurred while deleting the record');
+      return throwError(e);
+    }
   }
 }
