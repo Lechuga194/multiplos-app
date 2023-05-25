5b03bb550f1b0af255d4e5af681de019c7b5c092
Lechuga194 <joselechuga194@gmail.com>
2023-05-25 00:00:24 -0600

    Feature: Adds base list endpoint

--- src/app/multiple-records/multiple-records.component.ts
+++ src/app/multiple-records/multiple-records.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MultipleService } from '@app/services/multiple.service';
+import { Multiple } from '@app/interfaces/multiple';
+import { sortArrayOfObjectsByNumericProperty } from '@app/utils/Arrays';
 
 @Component({
   selector: 'app-multiple-records',
@@ -6,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./multiple-records.component.scss'],
 })
 export class MultipleRecordsComponent implements OnInit {
-  constructor() {}
+  constructor(private multipleService: MultipleService) {}
+  records: Multiple[] = [];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.multipleService.getRecords().subscribe((records) => {
+      this.records = sortArrayOfObjectsByNumericProperty(records, 'number') as Multiple[];
+    });
+  }
 }
--- src/app/services/multiple.service.ts
+++ src/app/services/multiple.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { collection, Firestore, addDoc } from '@angular/fire/firestore';
+import { collection, Firestore, addDoc, collectionData } from '@angular/fire/firestore';
 import { Multiple } from '@app/interfaces/multiple';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,5 +11,10 @@ export class MultipleService {
   saveMultiple(multipleData: Multiple) {
     const multipleRef = collection(this.firestore, 'multiple');
     return addDoc(multipleRef, multipleData);
+  }
+
+  getRecords(): Observable<Multiple[]> {
+    const multipleRef = collection(this.firestore, 'multiple');
+    return collectionData(multipleRef) as Observable<Multiple[]>;
   }
 }
